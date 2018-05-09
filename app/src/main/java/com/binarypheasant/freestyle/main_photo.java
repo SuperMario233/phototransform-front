203468b376dcfc162818d6c406b38a039089b35e
XiaoZheng <1500012822@pku.edu.cn>
2018-05-09 20:58:30 +0800

    Achieve user log in, add user interface.

--- app/src/main/java/com/binarypheasant/freestyle/main_photo.java
+++ app/src/main/java/com/binarypheasant/freestyle/main_photo.java
@@ -1,13 +1,27 @@
 package com.binarypheasant.freestyle;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class main_photo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main_photo);
+
+        Button userButton = (Button) findViewById(R.id.userButton);
+        userButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    Intent GotoLogIn = new Intent(main_photo.this, log_in.class);
+                    startActivity(GotoLogIn);
+            }
+        });
     }
 }
