24a9dc41dfc4ad7671f5e3a958cd89e06faf10d7
liuxiangyu <doudoujiang98@sina.com>
2018-05-27 16:18:23 +0800

    准备merge

---
+++ app/src/main/java/com/binarypheasant/freestyle/profile_main.java
@@ -0,0 +1,13 @@
+package com.binarypheasant.freestyle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class profile_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_main);
+    }
+}
